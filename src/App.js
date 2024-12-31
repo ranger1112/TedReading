5784f91c9494e25aa02ce946e99e5438adb2924b
ranger <1648186705@qq.com>
2024-12-31 18:20:09 +0800

    feat:井字棋

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import MyApp from "./components/MyApp";
+import Board from "./components/Board";
 
-let user = {
-    name: "John",
-}
+
 
 function App() {
     return (
         <div className="App">
-            <img src={logo} className="App-logo" alt=""/>
-            <h1>{user.name}</h1>
-            <MyApp></MyApp>
+            <Board></Board>
         </div>
     );
 }
