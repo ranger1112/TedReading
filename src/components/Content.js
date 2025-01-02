54f5b9dd55282e629dd093d15e8d3dd9fcc475af
ranger <1648186705@qq.com>
2025-01-02 18:01:36 +0800

    feat:article title

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,18 @@
 export default function Content() {
     return (
-        <div className="h-full w-full border-r border-l border-gray-300 bg-blue-400">
-
+        <div className="flex flex-row justify-center">
+            {/*<div className="min-h-max px-3 py-5 w-full basis-3/5 border-r border-l border-gray-300 bg-blue-400">*/}
+            <div className="min-h-max px-8 py-5 w-full basis-3/5">
+                <h1 className="text-3xl font-bold text-gray-800">Don't believe everything you think</h1>
+                <div className="text-xl font-bold text-gray-600">那些限制你的执念是你的不幸之源</div>
+                <div className="text-sm text-gray-600 mt-2 pb-1 border-b border-gray-300">
+                    <span className="mr-5 hover:text-gray-900 hover:cursor-pointer">Lauren Weinstin</span>
+                    <span className="hover:text-blue-400 hover:cursor-pointer">
+                        <a href="https://www.ted.com/talks/lauren_weinstein_don_t_believe_everything_you_think">阅读原文</a>
+                    </span>
+                </div>
+            </div>
         </div>
+
     )
 }
