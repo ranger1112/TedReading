88512542078b4f05075f02ad25fb5b78b8dee49b
ranger <1648186705@qq.com>
2025-01-02 15:37:03 +0800

    feat: header

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,10 @@
 export default function Head() {
 
     return (
-        <div className="min-h-14 w-full border-b border-gray-300 flex justify-center items-center space-x-4">
-            <div className="p-6 min-h-14 w-30 content-center">Recommend</div>
-            <div className="p-6 min-h-14 w-30 content-center">Articles</div>
-            <div className="p-6 min-h-14 w-30 content-center">Communication</div>
+        <div className="min-h-14 pl-64 w-full border-b border-gray-300 flex justify-start items-center space-x-4">
+            <div className="p-6 min-h-14 w-30 font-semibold text-gray-900 content-center hover:bg-orange-500 hover:text-white">Recommend</div>
+            <div className="p-6 min-h-14 w-30 font-semibold text-gray-900 content-center hover:bg-orange-500 hover:text-white">Articles</div>
+            <div className="p-6 min-h-14 w-30 font-semibold text-gray-900 content-center hover:bg-orange-500 hover:text-white">Communication</div>
         </div>
     )
 }
