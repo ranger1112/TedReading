6d3582cb62b05a7cf84e09ffacfa254fb7c331c8
weicheng <1648186705@qq.com>
2025-01-02 21:51:26 +0800

    feat: article

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,23 @@
 export default function Footer() {
-    return (<div>Footer</div>)
+    return (
+        <div className="flex flex-row justify-center border-t border-gray-300">
+            <div className="min-h-max px-8 py-5 w-full basis-3/5 space-x-4">
+                <span className="text-gray-600 text-sm font-semibold hover:text-gray-900">
+                    &copy; 2025 Ted Reading
+                </span>
+                <span className="text-gray-600 text-sm font-semibold hover:text-gray-900">
+                    About Us
+                </span>
+                <span className="text-gray-600 text-sm font-semibold hover:text-gray-900">
+                    License
+                </span>
+                <span className="text-gray-600 text-sm font-semibold hover:text-gray-900">
+                    Contribute
+                </span>
+                <span className="text-gray-600 text-sm font-semibold hover:text-gray-900">
+                    Contact Us
+                </span>
+            </div>
+        </div>
+    );
 }
